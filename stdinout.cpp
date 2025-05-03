107a44c2c0aebb044ef53eebf2f5ccb907d2f7a3
OmarSamehMahmoud <omarsamehmahmoud@gmail.com>
2025-05-04 00:55:30 +0300

    std input output example

---
+++ stdinout.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void){
+    string name;
+    cout << "Hello Modern c++" << endl;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "My name is: " << name << endl;
+}
